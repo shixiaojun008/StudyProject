6aa3d1396a33ccdd21e8368a504b63c3baceb6fd
jerry <shixiaojun008@163.com>
2020-03-20 16:46:33 +0800

    update new file.

---
+++ ClassTest.py
@@ -0,0 +1,106 @@
+# class ClassTest:
+#     def prt(self):
+#         print(self)
+#         print(self.__class__)
+#
+#
+# t = ClassTest()
+# t.prt()
+
+
+# 类定义
+class people:
+    # 定义基本属性
+    name = ''
+    age = 0
+    # 定义私有属性,私有属性在类外部无法直接进行访问
+    __weight = 0
+
+    # 定义构造方法
+    def __init__(self, n, a, w):
+        self.name = n
+        self.age = a
+        self.__weight = w
+
+    def speak(self):
+        print("%s 说: 我 %d 岁。" % (self.name, self.age))
+
+
+# 单继承示例
+class student(people):
+    grade = ''
+
+    def __init__(self, n, a, w, g):
+        # 调用父类的构函
+        people.__init__(self, n, a, w)
+        self.grade = g
+
+    # 覆写父类的方法
+    def speak(self):
+        print("%s 说: 我 %d 岁了，我在读 %d 年级" % (self.name, self.age, self.grade))
+
+
+# 另一个类，多重继承之前的准备
+class speaker():
+    topic = ''
+    name = ''
+
+    def __init__(self, n, t):
+        self.name = n
+        self.topic = t
+
+    def speak(self):
+        print("我叫 %s，我是一个演说家，我演讲的主题是 %s" % (self.name, self.topic))
+
+
+# 多重继承
+class sample(speaker, student):
+    a = ''
+
+    def __init__(self, n, a, w, g, t):
+        student.__init__(self, n, a, w, g)
+        speaker.__init__(self, n, t)
+
+
+test = sample("Tim", 25, 80, 4, "Python")
+test.speak()  # 方法名同，默认调用的是在括号中排前地父类的方法
+
+print("\n")
+
+class Parent:  # 定义父类
+    def myMethod(self):
+        print('调用父类方法')
+
+
+class Child(Parent):  # 定义子类
+    def myMethod(self):
+        print('调用子类方法')
+
+
+c = Child()  # 子类实例
+c.myMethod()  # 子类调用重写方法
+super(Child, c).myMethod()  # 用子类对象调用父类已被覆盖的方法
+
+#类的私有方法实例如下：
+
+class Site:
+    def __init__(self, name, url):
+        self.name = name  # public
+        self.__url = url  # private
+
+    def who(self):
+        print('name  : ', self.name)
+        print('url : ', self.__url)
+
+    def __foo(self):  # 私有方法
+        print('这是私有方法')
+
+    def foo(self):  # 公共方法
+        print('这是公共方法')
+        self.__foo()
+
+
+x = Site('菜鸟教程', 'www.runoob.com')
+x.who()  # 正常输出
+x.foo()  # 正常输出
+# x.__foo()  # 报错   私有方法，不能调用。

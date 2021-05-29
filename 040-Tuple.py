9d1b7b22bc62ae36ee3b5262755950a42296d10e
Administrator <shixiaojun008@163.com>
2021-05-29 15:59:27 +0800

    add new study files.

---
+++ 040-Tuple.py
@@ -0,0 +1,20 @@
+# 通过小括号创建
+
+tuple10 = (10, 20, 30)
+print(tuple10)
+
+print(type(tuple10))
+
+# 通过 tuple() 函数创建
+
+tuple20 = tuple("abc")
+tuple21 = tuple(range(5))
+tuple22 = tuple(["A", "C", "B"])
+
+print(tuple20)
+print(tuple21)
+print(tuple22)
+
+del tuple22
+
+

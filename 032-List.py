b4a285d4782e7ad2c5631191e4a749545d8b7db3
Administrator <shixiaojun008@163.com>
2021-05-29 15:49:07 +0800

    add new study files.

--- 032-List.py
+++ 032-List.py
@@ -1,4 +1,55 @@
-# var1 = 'JERRY'
+
+# list 的生成
+
+#通过list 生成
+
+#list1 = ["A", "B", "C"]
+list1 = list("ABC")
+print(list1)
+
+#通过 range 生成
+list2 =list(range(1, 50, 3))
+print(list2)
+
+#通过推导式生成列表
+
+list3 =[x*2 for x in range(5)]
+print (list3)
+
+list1.append("JERRY")
+print(list1)
+
+list2.extend(["TOM", "GO"])
+print(list2)
+
+
+list10 = [20, 40, 60, 10, 90, 70]
+list10.sort()
+print(list10)
+list11 = [20, 40, 60, 10, 90, 70]
+list11.sort(reverse=True)
+print(list11)
+
+
+list20 = [20, 10, 40, 30]
+reversedList20 = reversed(list20)
+list21 = list(reversedList20)
+print(list21)
+
+
+list30 = [
+    ["高小一",18,30000,"北京"],
+    ["高小二",19,40000, "上海"],
+    ["高小三",20,50000, "深圳"]
+]
+
+print(list30[1])
+
+for x in range(3):
+    print(list30[x])
+
+
+
 
 '''
 print ("var1[1:3]:", var1.capitalize()[0:3])

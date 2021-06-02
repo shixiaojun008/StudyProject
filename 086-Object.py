5d54ada632c6b5458959b0c9f3f70b96d2d43ea9
shixiaojun008 <42019609+shixiaojun008@users.noreply.github.com>
2021-06-02 12:25:47 +0800

    update.

--- 086-Object.py
+++ 086-Object.py
@@ -2,36 +2,77 @@ class Student:
     company = "Co.LT"  # 类属性
     count = 0  # 类属性
 
+    __score = 97
+
     def __init__(self, name, score):
         self.name = name
-        self.score = score
+        self.__score = score
 
     def say_score(self):
-        print("{0}的分数是：{1},到校次数是{2}".format(self.name, self.score, self.count))
+        print("{0}的分数是：{1},到校次数是{2}".format(self.name, self.__score, self.count))
 
     @classmethod
     def print_company(cls):
         print("公司是{0}".format(cls.company))
 
+    @property                   #Python 装饰器
+    def score(self):
+        print("Get score ...{0}".format(self.__score))
+        return self.__score
+
+    @score.setter      #Python 装饰器
+    def score(self, score):
+        self.__score = score
+
     @staticmethod
     def print_sum(n1, n2):
         print("到校次数是:{}".format(n1 + n2))
+
+    def __call__(self):
+        print("计算结果出来啦。。。。。，年薪为：{0}".format(30000))
+
+    def __PrintScore(self):
+        print("私有方法，打印私有属性值__score：{0}".format(self.__score))
 
     def __del__(self):
         print("销毁对象：{0}".format(self))
 
 
 class Man:
-    pass
+    sum1 = 100
+    sum2 = 200
 
+    def __call__(self):
+        print("计算结果出来啦。。。。。，年薪为：{0}".format(self.sum1 + self.sum2))
+        return self.sum1 + self.sum2
+
+    def work(self):
+        print("努力上班!")
+
+def n_work(m):
+        print("努力上班!赚钱娶媳妇！")
 
 s1 = Student("史沐晨", 98)
 s1.count = 20
 s1.say_score()
 s1.print_company()
 s1.print_sum(1, 2)
+s1()
+
+print("*****************************************************************")
+
+s1.score          # 像调用属性一样调用方法   @property
+#s1.salary = 30000  # 报错， 只能访问，不能赋值
+
+s1.score = 50000
+s1.score
+
+print("*****************************************************************")
 
 Student.print_company()
+
+print("私有属性值：{0}".format(s1._Student__score))      #  类的私有属性的访问
+s1._Student__PrintScore()
 
 print("*****************************************************************")
 
@@ -42,7 +83,6 @@ print(s2)
 s2.say_score()
 
 print("*****************************************************************")
-
 
 # 列出对象所有属性、方法
 print(dir(s1))
@@ -57,5 +97,13 @@ print(isinstance(s1, Man))
 
 print("*****************************************************************")
 
+sMan = Man()
+sMan()
+
+Man.work = n_work    # Man 类中的 work 方法被外部新的 n_work 方法动态替换为新的对象
+sMan.work()
+
+print("*****************************************************************")
+
 del s1
-#del s2   销毁s1的同时 对应的s2也被销毁，调用销毁方法
+# del s2   销毁s1的同时 对应的s2也被销毁，调用销毁方法
---
+++ 101-OO.py
@@ -0,0 +1,84 @@
+class Person:
+    __age = 18
+
+    def __init__(self, name, age):
+        self.name = name
+        self.__age = age
+
+    def getInfo(self):
+        print("姓名：{0}，年龄：{1}".format(self.name, self.__age))
+
+
+'''
+   
+    1、继承的用法， 继承后的初始化，私有属性的用法
+    
+
+'''
+
+
+class Student(Person):
+
+    def __init__(self, name, age, score):
+        Person.__init__(self, name, age)
+        self.score = score
+
+    def __str__(self):
+        return "重写 __str__ 方法，返回新的描述，类的名字是：Student"
+
+    def getInfo(self):
+        # 测试supper()，代表父类的定义，而不是父类的对象
+        super().getInfo()
+
+        # 子类自己的方法
+        print("姓名：{0}，年龄：{1}，成绩：{2}".format(self.name, self._Person__age, self.score))  # 父类的私有属性用法
+
+
+class Teacher(Person):
+
+    def __init__(self, name, age, salary):
+        Person.__init__(self, name, age)
+        self.salary = salary
+
+    def getInfo(self):
+        print("姓名：{0}，年龄：{1}，工资：{2}".format(self.name, self._Person__age, self.salary))  # 父类的私有属性用法
+
+
+stu01 = Student("史沐晨", 10, 100)
+stu01.getInfo()
+
+# 测试 __str__ 重写
+print(stu01)
+
+print("**" * 50)
+
+tch01 = Teacher("史小军", 30, 2000)
+tch01.getInfo()
+
+
+print("**" * 50)
+
+'''
+# 通过 mro()方法查看类的结构
+print(Student.mro())
+
+# 通过 dir()方法查看类的所有属性
+print(dir(stu01))
+
+# __dict__ 查看属性
+print(tch01.__dict__)
+
+# __class__ 查看所属类
+print(tch01.__class__)
+
+# __bases__ 查看父类
+print(Teacher.__bases__)
+
+# __subclasses__ 查看子类列表
+print(Person.__subclasses__())
+
+'''
+
+
+
+
